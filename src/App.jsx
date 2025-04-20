c0063aeb97e0fef67301b4b02cbf5a001de36aa4
dzikri <dzikri@ebconnect.com>
2025-04-21 00:11:40 +0700

    Revert "feat: add head"
    This reverts commit 9e4d74bd6eab3bbab3d03ee967688c135b1724f0.

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <h1>This is new feature</h1>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
